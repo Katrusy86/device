a22dca5ce2194b6fc9abac908b14a8a21e4bf1cc
Katrusy86 <39090902+Katrusy86@users.noreply.github.com>
2019-07-10 18:51:40 +0300

    updated the catalog section
    updated the catalog section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-    $('.slider_first_item').slick({
-        autoplay:true,
-     autoplaySpeed:2000,
-     dots:true
-      });         
-})
+    $('.popup').magnificPopup({
+ 
+    });
+});
