d2f9ed43ca6201d90cf5bc24f5b5ce94d072bfde
Katrusy86 <39090902+Katrusy86@users.noreply.github.com>
2019-08-12 22:17:08 +0300

    updated the search field
    updated the search field

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+  
+    $(".fa-search").click(function(){
+      $(".main-search, .main-search-name").toggleClass("active");
+      $("input[type='text']").focus();
+    });
     
-});
+  });
