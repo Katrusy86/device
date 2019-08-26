b30c2413284a11e3b6fc24ed17112f9d6e837bba
Katrusy86 <39090902+Katrusy86@users.noreply.github.com>
2019-08-26 14:03:03 +0300

    Updated the  pop-up form and pop-up map sections  and added js filies
    Updated the  pop-up form and pop-up map sections  and added js filies

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".fa-search").click(function(){
-      $(".main-search, .main-search-name,.act-search").toggleClass("active");
-      $("input[type='text']").focus();
-    });
-    
-  });
