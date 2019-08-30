2a6521b43d91c3d9868ffd2b07948a89bff36aa7
Katrusy86 <39090902+Katrusy86@users.noreply.github.com>
2019-08-31 01:21:03 +0300

    Updated google map for pop-up
    Updated google map for pop-up

--- js/map.js
+++ js/map.js
@@ -15,11 +15,3 @@ mapClose.addEventListener ("click",function (evt) {
     mapPopup.classList.remove("modal-show");
     overflow.classList.remove("in");
 });
-
-function initMap() {
-    var map1 = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: { lat: -33, lng: 151 },
-      disableDefaultUI: true
-    });
-}
