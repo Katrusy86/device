034efe7cde325bfbd5a7865da8869a66a6dffdeb
Katrusy86 <39090902+Katrusy86@users.noreply.github.com>
2019-09-03 18:38:43 +0300

    Changed padding to margin-right
    Changed padding to margin-right

--- js/map.js
+++ js/map.js
@@ -15,3 +15,19 @@ mapClose.addEventListener ("click",function (evt) {
     mapPopup.classList.remove("modal-show");
     overflow.classList.remove("in");
 });
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: {lat: 55.687222, lng: 37.529583},
+      disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+        position: {lat: 55.687222, lng: 37.529583},
+        map:map,
+        title: 'улица Строителей 15'
+    });
+
+  }
+
